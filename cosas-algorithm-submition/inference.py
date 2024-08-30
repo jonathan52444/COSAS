53a218b00f3096a6d36713e7390eeb3420cdf6e0
Mobarakol Islam <mobarakol@u.nus.edu>
2024-08-30 16:46:56 +0100

    Update inference.py

--- cosas-algorithm-submition/inference.py
+++ cosas-algorithm-submition/inference.py
@@ -7,6 +7,7 @@ import SimpleITK
 import torch
 
 from mmseg.apis import inference_model, init_model
+from PitSAM.segment_anything import sam_model_registry
 #from segment_anything import sam_model_registry, SamPredictor
 
 def read(path):
@@ -55,15 +56,20 @@ def main():
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
     # Load the SAM model
-    sam = sam_model_registry["vit_b"](checkpoint=sam_checkpoint)
-    sam.to(device=device)
-
+    # sam = sam_model_registry["vit_b"](checkpoint=sam_checkpoint)
+    sam, img_embedding_size = sam_model_registry[args.vit_name](image_size=256,
+                                                                    num_classes=2,
+                                                                    checkpoint=sam_checkpoint, pixel_mean=[0, 0, 0],
+                                                                    pixel_std=[1, 1, 1])
+    
+    pkg = import_module(args.module)
+    net = pkg.LoRA_Sam(sam, args.rank).cuda()
+    net.load_lora_parameters(lora_checkpoint)
+    net.to(device=device)
+    net.eval()
     # Load the LoRA checkpoint
-    lora_state_dict = torch.load(lora_checkpoint)
-    sam.load_state_dict(lora_state_dict, strict=False)
-
-    # Create a SamPredictor instance
-    predictor = SamPredictor(sam)
+    # lora_state_dict = torch.load(lora_checkpoint)
+    # sam.load_state_dict(lora_state_dict, strict=False)
 
     with torch.no_grad():
         for filename in os.listdir(input_root):
@@ -72,19 +78,27 @@ def main():
                 try:
                     input_path = input_root + '/' + filename
                     image = read(input_path)
+                    h,w,_ = image.shape
 
-                    # Set the image for the predictor
-                    predictor.set_image(image)
+                    outputs = net(image, True, patch_size[0])
+                    output_masks = outputs['masks']
+                    out = torch.argmax(torch.softmax(output_masks, dim=1), dim=1).squeeze(0)
+                    prediction = out.cpu().detach().numpy()
+                    if x != patch_size[0] or y != patch_size[1]:
+                        prediction = zoom(prediction, (w, h), order=0)
 
-                    # Generate masks (adjust the parameters as needed)
-                    masks, _, _ = predictor.predict(
-                        point_coords=None,
-                        point_labels=None,
-                        multimask_output=False,
-                    )
+                    # # Set the image for the predictor
+                    # predictor.set_image(image)
+
+                    # # Generate masks (adjust the parameters as needed)
+                    # masks, _, _ = predictor.predict(
+                    #     point_coords=None,
+                    #     point_labels=None,
+                    #     multimask_output=False,
+                    # )
 
                     # Save the generated mask
-                    write(output_path, masks[0].astype('uint8'))
+                    write(output_path, prediction.squeeze().numpy().astype('uint8'))
 
                 except Exception as error:
                     print(error)
